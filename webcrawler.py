6c676baa09df2389fecbbc8cbe6aecd636924cd7
naba90 <nabasmail@gmail.com>
2015-10-25 00:14:32 -0700

    Added functions to index and to remove unwanted characters while indexing.

--- webcrawler.py
+++ webcrawler.py
@@ -1,4 +1,8 @@
-#! /usr/bin/python2.7.8
+__author__ = 'Naba'
+
+
+# ! /usr/bin/python2.7.8
+
 
 # Function to get page source from link
 def get_page(url):
@@ -32,7 +36,7 @@ def union(p, q):
 def get_all_links(page):
     links = []
     while True:
-        url, endpos = get_next_target(page)  # Collect end posotion of page to iterate
+        url, endpos = get_next_target(page)  # Collect end position of page to iterate
         if url:
             links.append(url)
             page = page[endpos:]  # New page will begin from the end of the captured link.
@@ -41,32 +45,78 @@ def get_all_links(page):
     return links
 
 
-def crawl_web(seed, max_dept):
+def crawl_web(seed):
     """
 
     :rtype : list.
     """
     to_crawl = [seed]
     crawled = []
-    next_dept = []
-    dept = 0
-    while to_crawl and dept <= max_dept:  # limits the max dept in a particular link to be crawled.
+    index = []
+    while to_crawl:  # limits the max dept in a particular link to be crawled.
         # link = to_crawl.pop()  # Dept first algorithm
         link = to_crawl[0]
         to_crawl.remove(link)
         if link not in crawled:
-            # new_links = get_all_links(get_page(link))
-            # print "I am at newlinks\n"
-            # print new_links
-            union(next_dept, get_all_links(get_page(link)))  # To add links into next_dept w/o duplication
+            content = get_page(link)
+            add_page_to_index(index, link, content)
+            union(to_crawl, get_all_links(content))  # To add links into next_dept w/o duplication
             # to_crawl = union(to_crawl, new_links)
             crawled.append(link)
-        if not to_crawl:  # This updates to_crawl with links in next_dept
-            to_crawl, next_dept = next_dept, []
-            dept += 1
-    return crawled
+    return index
 
 
-# links = get_all_links(get_page("https://www.udacity.com/cs101x/index.html"))
-# print crawl_web("http://www.murtazachunia.com/index.html", 2)
-print crawl_web("https://www.udacity.com/cs101x/index.html", 1)
+# To record uses clicks
+def record_user_click(index, keyword, url):
+    urls = lookup(index, keyword)
+    if urls:
+        for entry in urls:
+            if entry[0] == url:
+                entry[1] += 1
+
+
+def add_to_index(index, keyword, url):
+    """
+
+    :type index: list
+    """
+    for entry in index:
+        # format of index : [[keyword, [[url, count], [url, count],...]],...]
+        if entry[0] == keyword:
+            for element in entry[1]:
+                if element[0] == url:
+                    return
+            entry[1].append([url,0])
+            return
+    # not found, add new keyword to index
+    index.append([keyword, [url,0]])
+
+
+# to update in index with all the word occurrences found in page
+def add_page_to_index(index, url, content):
+    words = []
+    atsplit = True
+    splitlist = " ';:?/<>,._+-={}[]|\n!@#$%^&*()"
+    for char in content:  # iterate through each character
+        if char in splitlist:
+            atsplit = True
+        else:
+            if atsplit:
+                words.append(char)
+                atsplit = False
+            else:
+                # at char to last word
+                words[-1] = words[-1] + char
+    #    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+
+def lookup(index, keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
+
+print crawl_web("https://www.udacity.com/cs101x/index.html")
